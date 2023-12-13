4b5e9171a03c72e1f3b0572d6e7154973783555a
User <stparchkg@gmail.com>
2023-12-13 21:18:47 +0600

    add ProductFacade

---
+++ purchases/src/main/java/kg/java/purchases/domain/facades/ProductFacadeImpl.java
@@ -0,0 +1,40 @@
+package kg.java.purchases.domain.facades;
+
+import jakarta.transaction.Transactional;
+import kg.java.purchases.core.exceptions.EntityNotFoundException;
+import kg.java.purchases.core.contracts.facades.ProductFacade;
+import kg.java.purchases.core.contracts.services.ProductService;
+import kg.java.purchases.core.exceptions.EntityDuplicateException;
+import kg.java.purchases.core.models.dtos.product.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class ProductFacadeImpl implements ProductFacade {
+    private final ProductService productService;
+
+    public ProductFacadeImpl(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @Override
+    public ProductDto add(CreateProductDto model) throws EntityDuplicateException {
+        return productService.add(model);
+    }
+
+    @Override
+    public ProductDto update(UpdateProductDto model) throws EntityNotFoundException {
+        return productService.update(model);
+    }
+
+    @Override
+    public HttpStatus delete(DeleteProductDto model) throws EntityNotFoundException {
+        return productService.delete(model);
+    }
+
+    @Override
+    public ProductDto findById(FindByIdProductDto model) throws EntityNotFoundException {
+        return productService.findById(model);
+    }
+}
