0f496a7d134316b2c65f13d57abc48c49f2c3cf2
User <stparchkg@gmail.com>
2023-12-13 20:38:21 +0600

    add BuyerService

---
+++ purchases/src/main/java/kg/java/purchases/core/contracts/services/BuyerService.java
@@ -0,0 +1,21 @@
+package kg.java.purchases.core.contracts.services;
+
+
+import kg.java.purchases.core.exceptions.EntityNotFoundException;
+import kg.java.purchases.core.exceptions.EntityDuplicateException;
+import kg.java.purchases.core.models.dtos.buyer.*;
+import org.springframework.http.HttpStatus;
+
+
+public interface BuyerService {
+    BuyerDto register(CreateBuyerDto model) throws EntityDuplicateException;
+
+    BuyerDto update(UpdateBuyerDto model) throws EntityNotFoundException;
+
+    HttpStatus delete(DeleteBuyerDto model) throws EntityNotFoundException;
+
+    BuyerDto findById(FindByIdBuyerDto model) throws EntityNotFoundException;
+
+
+
+}
