ee53a10ccb9a55f68ae5e8ef7f8a8ab05ff808de
User <stparchkg@gmail.com>
2023-12-13 22:13:37 +0600

    add method findByBuyerName in PurchaseFacade

--- purchases/src/main/java/kg/java/purchases/core/contracts/facades/PurchaseFacade.java
+++ purchases/src/main/java/kg/java/purchases/core/contracts/facades/PurchaseFacade.java
@@ -5,10 +5,13 @@ import kg.java.purchases.core.exceptions.EntityNotFoundException;
 import kg.java.purchases.core.models.dtos.purchase.*;
 import org.springframework.http.HttpStatus;
 
+import java.util.List;
+
 public interface PurchaseFacade {
     PurchaseDto add(CreatePurchaseDto model) throws EntityNotFoundException;
     PurchaseDto update(UpdatePurchaseDto model) throws EntityDuplicateException, EntityNotFoundException;
     HttpStatus delete(DeletePurchaseDto model) throws EntityNotFoundException;
     PurchaseDto findById(FindByIdPurchaseDto model) throws EntityNotFoundException;
+    List<PurchaseDto> findByBuyerName(FindPurchasesByBuyerNameDto model);
 
 }
