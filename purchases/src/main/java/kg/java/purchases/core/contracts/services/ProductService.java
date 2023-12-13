23c0f2bdd9c3c4e737e0785985c10eea671bb1d6
User <stparchkg@gmail.com>
2023-12-13 21:16:46 +0600

    add ProductService

---
+++ purchases/src/main/java/kg/java/purchases/core/contracts/services/ProductService.java
@@ -0,0 +1,17 @@
+package kg.java.purchases.core.contracts.services;
+
+
+import kg.java.purchases.core.exceptions.EntityNotFoundException;
+import kg.java.purchases.core.exceptions.EntityDuplicateException;
+import kg.java.purchases.core.models.dtos.product.*;
+import org.springframework.http.HttpStatus;
+
+public interface ProductService {
+    ProductDto add(CreateProductDto model) throws EntityDuplicateException;
+
+    ProductDto update(UpdateProductDto model) throws EntityNotFoundException;
+
+    HttpStatus delete(DeleteProductDto model) throws EntityNotFoundException;
+
+    ProductDto findById(FindByIdProductDto model) throws EntityNotFoundException;
+}
