6b2981e823245cdeb93f61fe6f29b9529f3229a0
User <stparchkg@gmail.com>
2023-12-13 20:56:36 +0600

    add ProductRepository

---
+++ purchases/src/main/java/kg/java/purchases/data/ProductRepository.java
@@ -0,0 +1,12 @@
+package kg.java.purchases.data;
+
+import kg.java.purchases.core.models.entities.ProductEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductEntity,Long> {
+    Optional<ProductEntity> findByName(String name);
+}
